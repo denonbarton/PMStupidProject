cf910b20403ed6bd51ee076525487a70e626c4de
denonbarton <dbar0540@740s-pl-126.local>
2015-09-25 13:17:57 -0600

    Finally works!
    hey yeah

--- app/src/main/java/ctec/pmstupidproject/StupidAfternoonActivity.java
+++ app/src/main/java/ctec/pmstupidproject/StupidAfternoonActivity.java
@@ -7,34 +7,40 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.RelativeLayout;
+import android.view.View;
+import android.graphics.Color;
+
 
 public class StupidAfternoonActivity extends AppCompatActivity
 {
-    private Button corChangeButton;
-    private RelativeLayout background;
-    private TextView;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
+    private Button colorChangeButton;
+    private RelativeLayout background;
+    private TextView sillyWords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stupid_afternoon);
+        setContentView(R.layout.activity_stupid);
 
         colorChangeButton = (Button) findViewById(R.id.sillyButton);
-            background = (relativeLayout) findViewById(R.id.appBackground);
-            sillyWords = (TextView) findViewById(R.id.sillyWords);
+        background = (RelativeLayout) findViewById(R.id.appBackground);
+        sillyWords = (TextView) findViewById(R.id.sillyWords);
+
+        setupListeners();
+
+
     }
 
-        private Button colorChangeButton;
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stupid_afternoon, menu);
+        getMenuInflater().inflate(R.menu.menu_stupid, menu);
         return true;
     }
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -47,5 +53,40 @@ public class StupidAfternoonActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void changeColors()
+    {
+        int redColor;
+        int greenColor;
+        int blueColor;
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        background.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        colorChangeButton.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
     }
+
+    private void setupListeners()
+    {
+        colorChangeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View buttonView)
+            {
+                //This is where you put code that happens when you click a button.
+                changeColors();
+            }
+
+
+        });
+    }
+
 }
