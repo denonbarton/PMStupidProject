29dfbfa55e64c0a8aea674666e3a7f81cd00b8e9
denonbarton <dbar0540@740s-pl-126.local>
2015-09-25 12:21:33 -0600

    added new stuff

--- app/src/main/java/ctec/pmstupidproject/StupidAfternoonActivity.java
+++ app/src/main/java/ctec/pmstupidproject/StupidAfternoonActivity.java
@@ -4,24 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
 
-public class StupidAfternoonActivity extends AppCompatActivity {
+public class StupidAfternoonActivity extends AppCompatActivity
+{
+    private Button corChangeButton;
+    private RelativeLayout background;
+    private TextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stupid_afternoon);
+
+        colorChangeButton = (Button) findViewById(R.id.sillyButton);
+            background = (relativeLayout) findViewById(R.id.appBackground);
+            sillyWords = (TextView) findViewById(R.id.sillyWords);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+        private Button colorChangeButton;
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_stupid_afternoon, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -33,5 +46,6 @@ public class StupidAfternoonActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
     }
 }
